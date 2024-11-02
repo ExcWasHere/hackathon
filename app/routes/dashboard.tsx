0b13c2ef7a3d20fb951d15e33cb768da3aa4bd5e
emkafie <khafi.farel77@gmail.com>
2024-11-02 11:12:35 +0700

    Dashboard-punk

---
+++ app/routes/dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import DashboardPage from '~/components/dashboard/hero'
+
+export default function Dashboard() {
+  return (
+    <>
+    <DashboardPage />
+    </>
+  )
+}
